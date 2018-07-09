8d99e6b272dabcb2d6073d550ba4cf6a4947ad36
jingan-woodpecker <liaojingan1995@gmail.com>
2018-07-09 15:26:18 +0800

    切换浏览器窗口句柄

---
+++ 06SwitchingWindows.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+import unittest
+
+class SwitchingWindowByChrome(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        #隐式等待
+        self.driver.implicitly_wait(10)
+
+    def test_operateWindowHandle(self):
+        url = "http://www.baidu.com"
+        self.driver.get(url)
+        self.driver.maximize_window()
+        #获取当前窗口句柄
+        currentWindow = self.driver.current_window_handle
+        print(currentWindow)
+        #点击百度页面“登录”链接文本
+        self.driver.find_element_by_link_text("登录").click()
+        #点击弹窗中的“立即注册”链接文本
+        self.driver.find_element_by_link_text("立即注册").click()
+        #获取所有窗口的句柄
+        allHandles = self.driver.window_handles
+        #判断此页面不是百度首页窗口，并进入到注册窗口
+        for handle in allHandles:
+            if handle != currentWindow:
+                self.driver.switch_to.window(handle)
+                print("Registration window")
+                #判断页面中存在关键字“用户名”
+                assert "用户名" in self.driver.page_source, "不存在该关键字"
+
+        #判断是百度首页并使用访问列表方式进入
+        for handle in allHandles:
+            if handle == currentWindow:
+                self.driver.switch_to.window(allHandles[0])
+                print("Current window")
+                self.driver.find_element_by_id("TANGRAM__PSP_4__closeBtn").click()
+                #判断页面存在关键字“新闻”
+                assert "地图" in self.driver.page_source, "不存在该关键字"
+
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
