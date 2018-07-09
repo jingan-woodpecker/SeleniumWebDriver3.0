1c6c1a1992a7bd6ddc57c663dc3df1da68195c49
jingan-woodpecker <liaojingan1995@gmail.com>
2018-07-09 13:53:44 +0800

    获取当前页面的URL地址

---
+++ GetCurrentUrl04.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import unittest
+
+class GetCurrentPageUrlByChrome(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_getCurrenPageUrl(self):
+        url = "https://www.baidu.com/"
+        self.driver.get(url)
+        self.driver.maximize_window()
+        #获取当前页面的URL
+        currentPageURL = self.driver.current_url
+        print(currentPageURL)
+        #断言当前网址是否为"https://www.baidu.com/"
+        self.assertEqual(currentPageURL, "https://www.baidu.com/", "当前网址不正确")
+        
+    def tearDown(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
