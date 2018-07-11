2f54a8ae1094869b8a2866849d171f0fc0280116
jingan-woodpecker <liaojingan1995@gmail.com>
2018-07-11 16:50:33 +0800

    模拟键盘单个按键操作

---
+++ 24AnalogButtonOperation.py
@@ -0,0 +1,32 @@
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+import unittest, time
+
+class AnalogButtonByChrome(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+
+    def test_operateButton(self):
+        url = "http://www.baidu.com"
+        self.driver.get(url)
+        self.driver.maximize_window()
+        kw = self.driver.find_element_by_id("kw")
+        #通过webdriver实例发送一个F12键
+        kw.send_keys(Keys.F12)
+        time.sleep(3)
+        #再次模拟发送一个F12键
+        kw.send_keys(Keys.F12)
+        #在搜索输入框输入“自动化测试”
+        kw.send_keys("自动化测试")
+        #模拟发送一个回车键
+        kw.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+    def tearDown(self):
+        # 退出浏览器
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
